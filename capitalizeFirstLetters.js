1eeee4f236b86add197a9a991a5788697ee12aee
Fabrice <dupont.fabrice62@gmail.com>
2023-03-20 12:49:43 +0300

    using mocha as test runner

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,5 +1,3 @@
-const assert = require('assert')
-
 function capitalizeFirstLetters(input) {
   return input.length > 0
     ? input
@@ -9,29 +7,4 @@ function capitalizeFirstLetters(input) {
     : ''
 }
 
-// Check that capitalizeFirst is a function
-
-assert.strictEqual(typeof capitalizeFirstLetters, 'function')
-
-// Check that capitalizeFirst accepts one argument
-
-assert.strictEqual(capitalizeFirstLetters.length, 1)
-
-// Check that capitalizeFirst transforms javaScript correctly
-
-assert.strictEqual(capitalizeFirstLetters('javaScript'), 'JavaScript')
-
-// Check that capitalizeFirst transforms "i am learning TDD" correctly
-
-assert.strictEqual(
-  capitalizeFirstLetters('i am learning TDD'),
-  'I Am Learning TDD'
-)
-
-// Check that it works for a 1-character string
-
-assert.strictEqual(capitalizeFirstLetters('z'), 'Z')
-
-// Check that it works for an empty string
-
-assert.strictEqual(capitalizeFirstLetters(''), '')
+module.exports = capitalizeFirstLetters
